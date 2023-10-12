4f01398a84edfc51cd3bf53f5daf973fa1049fee
walicharamsey <ramsey@Cullen-PC.localdomain>
2023-10-12 08:52:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+
+const drivers = [
+    "John",
+    "Alice",
+    "Bob",
+    "Charlie"
+];
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
